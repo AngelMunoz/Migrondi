921e523953d4b7de6534bccd8267feb4d88fd8eb
Angel D. Munoz <scythes.lol@gmail.com>
2020-04-05 19:18:21 -0600

    Add CommandLine Verbs and Options

---
+++ Options.fs
@@ -0,0 +1,28 @@
+namespace Sqlator
+
+open CommandLine
+
+module Options =
+    [<Verb("new", HelpText = "Creates a new Migration file.")>]
+    type NewOptions =
+        { [<Option('n', "name", Required = true, HelpText = "Friendly Name of the Migration you want to create.")>]
+          name: string }
+
+    [<Verb("up", HelpText = "Runs the migrations against the database.")>]
+    type UpOptions =
+        { [<Option('t', "total", Required = false, HelpText = "Amount of migrations to run up.")>]
+          total: int option }
+
+    [<Verb("down", HelpText = "Rolls back migrations from the database.")>]
+    type DownOptions =
+        { [<Option('t', "total", Required = false, HelpText = "Amount of migrations to run down.")>]
+          total: int option }
+
+    [<Verb("list", HelpText = "List the amount of migrations in the database.")>]
+    type ListOptions =
+        { [<Option('t', "total", Required = false, HelpText = "Shows every migration ran against the database.")>]
+          total: bool
+          [<Option('m', "missing", Required = false, HelpText = "Shows the migrations that are pending to run.")>]
+          missing: bool
+          [<Option('l', "last", Required = false, HelpText = "Shows the last migration run agains the database.")>]
+          last: int option }
--- Program.fs
+++ Program.fs
@@ -1,10 +1,27 @@
-﻿open System
+﻿open CommandLine
 open FSharp.Data.Dapper
-open Sqlator.Migrations
+open Sqlator.Options
 
-    [<EntryPoint>]
-    let main argv =
-        OptionHandler.RegisterTypes()
-        let results = asyncRunMigrations()
-        printfn "%A" results
-        0 // return an integer exit code
+
+[<EntryPoint>]
+let main argv =
+
+  OptionHandler.RegisterTypes()
+  let result = CommandLine.Parser.Default.ParseArguments<NewOptions, UpOptions, DownOptions, ListOptions>(argv)
+  match result with
+  | :? Parsed<obj> as command ->
+    match command.Value with 
+    | :? NewOptions as newOptions ->
+            printfn "newOptions: %A" newOptions
+    | :? UpOptions as upOptions ->
+            printfn "upOptions: %A" upOptions
+    | :? DownOptions as downOptions ->
+            printfn "downOptions: %A" downOptions
+    | :? ListOptions as listOptions ->
+            printfn "listOptions: %A" listOptions
+    | _ -> failwith "Unexpected parsing result"
+  | :? NotParsed<obj> as notParsed ->
+    printfn "Not Parsed: %A" (notParsed.Errors |> Seq.map(fun err -> err.Tag))
+  | _ -> failwith "Unexpected parsing result"
+
+  0
