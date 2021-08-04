c02b789a35224dd006cf80801fc7fe1e56fb569b
Angel D. Munoz <scythes.lol@gmail.com>
2021-08-04 16:05:23 -0500

    preparing to add no-color and json output options

--- src/Migrondi/Options.fs
+++ src/Migrondi/Options.fs
@@ -4,17 +4,31 @@ module Options =
     open System
     open CommandLine
 
-    [<CLIMutable;Verb("init", HelpText = "Creates basic files and directories to start using migrondi.")>]
+    [<CLIMutable; Verb("init", HelpText = "Creates basic files and directories to start using migrondi.")>]
     type InitOptions =
         { [<Option('p', "path", Required = false, HelpText = "Where should the migrondi.json should be created")>]
-          path: string }
+          path: string
+          [<Option("no-color",
+                   Required = false,
+                   HelpText = "Write to the console without coloring enabled",
+                   Default = false)>]
+          noColor: bool
+          [<Option('j', "json", Required = false, HelpText = "Output to the console with a json format", Default = false)>]
+          json: bool }
 
-    [<CLIMutable;Verb("new", HelpText = "Creates a new Migration file.")>]
+    [<CLIMutable; Verb("new", HelpText = "Creates a new Migration file.")>]
     type NewOptions =
         { [<Option('n', "name", Required = true, HelpText = "Friendly Name of the Migration you want to create.")>]
-          name: string }
+          name: string
+          [<Option("no-color",
+                   Required = false,
+                   HelpText = "Write to the console without coloring enabled",
+                   Default = false)>]
+          noColor: bool
+          [<Option('j', "json", Required = false, HelpText = "Output to the console with a json format", Default = false)>]
+          json: bool }
 
-    [<CLIMutable;Verb("up", HelpText = "Runs the migrations against the database.")>]
+    [<CLIMutable; Verb("up", HelpText = "Runs the migrations against the database.")>]
     type UpOptions =
         { [<Option('t', "total", Required = false, HelpText = "Amount of migrations to run up.", Default = 1)>]
           total: int
@@ -23,9 +37,16 @@ module Options =
                    Required = false,
                    HelpText = "Prints to the console what is going to be run against the database",
                    Default = true)>]
-          dryRun: bool }
+          dryRun: bool
+          [<Option("no-color",
+                   Required = false,
+                   HelpText = "Write to the console without coloring enabled",
+                   Default = false)>]
+          noColor: bool
+          [<Option('j', "json", Required = false, HelpText = "Output to the console with a json format", Default = false)>]
+          json: bool }
 
-    [<CLIMutable;Verb("down", HelpText = "Rolls back migrations from the database.")>]
+    [<CLIMutable; Verb("down", HelpText = "Rolls back migrations from the database.")>]
     type DownOptions =
         { [<Option('t', "total", Required = false, HelpText = "Amount of migrations to run down.", Default = 1)>]
           total: int
@@ -34,13 +55,39 @@ module Options =
                    Required = false,
                    HelpText = "Prints to the console what is going to be run against the database",
                    Default = true)>]
-          dryRun: bool }
+          dryRun: bool
+          [<Option("no-color",
+                   Required = false,
+                   HelpText = "Write to the console without coloring enabled",
+                   Default = false)>]
+          noColor: bool
+          [<Option('j', "json", Required = false, HelpText = "Output to the console with a json format", Default = false)>]
+          json: bool }
 
-    [<CLIMutable;Verb("list", HelpText = "List the amount of migrations in the database.")>]
+    [<CLIMutable; Verb("list", HelpText = "List the amount of migrations in the database.")>]
     type ListOptions =
-        { [<Option('a', "all", Required = false, HelpText = "Shows every migration ran against the database.", Default = false)>]
+        { [<Option('a',
+                   "all",
+                   Required = false,
+                   HelpText = "Shows every migration ran against the database.",
+                   Default = false)>]
           all: bool
-          [<Option('m', "missing", Required = false, HelpText = "Shows the migrations that are pending to run.", Default = false)>]
+          [<Option('m',
+                   "missing",
+                   Required = false,
+                   HelpText = "Shows the migrations that are pending to run.",
+                   Default = false)>]
           missing: bool
-          [<Option('l', "last", Required = false, HelpText = "Shows the last migration run agains the database.", Default = true)>]
-          last: bool }
+          [<Option('l',
+                   "last",
+                   Required = false,
+                   HelpText = "Shows the last migration run agains the database.",
+                   Default = true)>]
+          last: bool
+          [<Option("no-color",
+                   Required = false,
+                   HelpText = "Write to the console without coloring enabled",
+                   Default = false)>]
+          noColor: bool
+          [<Option('j', "json", Required = false, HelpText = "Output to the console with a json format", Default = false)>]
+          json: bool }
---
+++ src/Migrondi/Writer.fs
@@ -0,0 +1,89 @@
+﻿namespace Migrondi.Writer
+
+open System.Text.Json
+open System.Text.Json.Serialization
+open Spectre.Console
+
+
+type ConsoleOutput =
+    | Normal of string
+    | Warning of string
+    | Danger of string
+    | Success of string
+
+type JsonOutput =
+    { FullContent: string
+      Parts: ConsoleOutput list option }
+
+type MigrondiOutput =
+    | ConsoleOutput of ConsoleOutput list
+    | JsonOutput of JsonOutput
+
+type JsonWriter = JsonOutput -> string
+type ConsoleWriter = ConsoleOutput list -> string
+
+type MigrondiWriter = MigrondiOutput -> string
+
+[<RequireQualifiedAccess>]
+module Writer =
+
+    let Writer
+        (jsonWriter: JsonOutput -> string)
+        (consoleWriter: ConsoleOutput list -> string)
+        (output: MigrondiOutput)
+        =
+        match output with
+        | JsonOutput output -> jsonWriter output
+        | ConsoleOutput output -> consoleWriter output
+
+    let private jsonOptions =
+        lazy
+            (fun () ->
+                let opts = JsonSerializerOptions()
+                opts.Converters.Add(JsonFSharpConverter())
+                opts.DefaultIgnoreCondition <- JsonIgnoreCondition.WhenWritingNull
+                opts)
+
+    let private jsonWriter: JsonWriter =
+        fun json -> JsonSerializer.Serialize(json, jsonOptions.Value())
+
+    let private coloredConsoleWriter: ConsoleWriter =
+        fun parts ->
+            let colored =
+                parts
+                |> List.map
+                    (function
+                    | Normal value -> value
+                    | Warning value -> $"[yellow]{value}[/]"
+                    | Danger value -> $"[red]{value}[/]"
+                    | Success value -> $"[green]{value}[/]")
+
+            System.String.Join("", colored)
+
+    let private noColorConsoleWriter: ConsoleWriter =
+        fun parts ->
+            let colored =
+                parts
+                |> List.map
+                    (function
+                    | Normal value
+                    | Warning value
+                    | Danger value
+                    | Success value -> value)
+
+            System.String.Join("", colored)
+
+    let GetMigrondiWriter (withColor: bool) : MigrondiWriter =
+        if withColor then
+            Writer jsonWriter coloredConsoleWriter
+        else
+            Writer jsonWriter noColorConsoleWriter
+
+type MigrondiConsole() =
+    static member Log(output: MigrondiOutput, ?withColor: bool, ?withWriter: MigrondiWriter) =
+        let withColor = defaultArg withColor true
+
+        let writer =
+            defaultArg withWriter (Writer.GetMigrondiWriter withColor)
+
+        AnsiConsole.Markup(writer output)
