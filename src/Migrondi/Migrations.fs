51158f9372a178873848cdcc421860b8e2c8d77c
Angel D. Munoz <scythes.lol@gmail.com>
2021-08-05 20:07:22 -0500

    update to new structure allow migration fns to be partially applied

--- src/Migrondi/Migrations.fs
+++ src/Migrondi/Migrations.fs
@@ -1,178 +1,222 @@
 namespace Migrondi
 
-open System
 open System.Data
 open Migrondi.Types
-open Options
-open Utils
-open Queries
+open Migrondi.FileSystem
+open Migrondi.Writer
+open Migrondi.Queries
+open Migrondi.Options
+
 open FsToolkit.ErrorHandling
-open System.IO
-open System.Text.Json
 
 module Migrations =
 
-    let runMigrationsInit (opts: InitOptions) : Result<string * string, exn> =
+    let tryRunMigrationsInit
+        (tryGetOrCreateDirectory: TryGetOrCreateDirectoryFn)
+        (tryGetOrCreateConfig: TryGetOrCreateConfigFn)
+        (opts: InitOptions)
+        : Result<int, exn> =
         result {
-            let! dir = getOrCreateMigrationsDir opts.path
-            let migrationsPath = Path.Combine(dir.FullName, "migrations")
-
-            use! file =
-                try
-                    Ok
-                    <| File.Create(Path.Combine(opts.path, "migrondi.json"))
-                with
-                | ex -> Error ex
-
-            let content =
-                let opts = JsonSerializerOptions()
-                opts.WriteIndented <- true
-                opts.PropertyNamingPolicy <- JsonNamingPolicy.CamelCase
-
-                JsonSerializer.SerializeToUtf8Bytes(
-                    { connection = "Data Source=migrondi.db"
-                      migrationsDir = migrationsPath
-                      driver = "sqlite" },
-                    opts
-                )
-
-            try
-                file.Write(ReadOnlySpan<byte>(content))
-            with
-            | ex -> do! Error ex
-
-            return (file.Name, migrationsPath)
+            let! migrationsDir =
+                tryGetOrCreateDirectory opts.path
+                |> Result.map (tryGetOrCreateConfig "migrondi.json")
+            // TODO: Print config and migrationsDir
+            return 0
         }
 
+    let tryRunMigrationsNew
+        (createMigration: TryCreateFileFn)
+        (config: MigrondiConfig)
+        (options: NewOptions)
+        : Result<int, exn> =
+        let logCreated (name: string) =
+            let createdMsg =
+                [ ConsoleOutput.Success $"Created: {name}\n" ]
 
 
-    let runMigrationsNew (path: string, _: MigrondiConfig, _: Driver) (options: NewOptions) =
-        let file, bytes =
-            createNewMigrationFile path options.name
+            if options.json then
+                MigrondiConsole.Log(
+                    JsonOutput.FromParts createdMsg
+                    |> MigrondiOutput.JsonOutput
+                )
+            else
+                MigrondiConsole.Log(createdMsg |> MigrondiOutput.ConsoleOutput, options.noColor |> not)
 
-        file.Write(ReadOnlySpan<byte>(bytes))
-        printfn $"""Created: {file.Name}"""
-        file.Dispose()
+            0
 
-    let runMigrationsUp
-        (connection: IDbConnection)
-        (path: string, config: MigrondiConfig, driver: Driver)
+        let initMessage =
+            [ ConsoleOutput.Normal "Running Migrondi New\n" ]
+
+
+        if options.json then
+            MigrondiConsole.Log(
+                JsonOutput.FromParts initMessage
+                |> MigrondiOutput.JsonOutput
+            )
+        else
+            MigrondiConsole.Log(initMessage |> MigrondiOutput.ConsoleOutput, options.noColor |> not)
+
+        createMigration config.migrationsDir options.name
+        |> Result.map logCreated
+
+    let tryRunMigrationsUp
         (options: UpOptions)
+        (config: MigrondiConfig)
+        (getConnection: string -> Driver -> Lazy<IDbConnection>)
+        (getMigrations: TryGetMigrationsFn)
         =
-        let created = ensureMigrationsTable driver connection
+        result {
+            let driver = Driver.FromString config.driver
+            let connection = getConnection config.connection driver
 
-        if not created then
-            raise (UnauthorizedAccessException "Could not create the Database/Migrations table")
+            do! ensureMigrationsTable driver connection.Value
 
-        let migration = getLastMigration connection
+            let! migration = getLastMigration connection.Value
+            let migrations = getMigrations config.migrationsDir
 
-        let migrations = getMigrations path
-
-        let pendingMigrations =
-            getPendingMigrations migration migrations
-
-        let migrationsToRun =
-            match options.total with
-            | total when total > 0 ->
-                let total =
-                    match total >= pendingMigrations.Length with
-                    | true -> pendingMigrations.Length - 1
-                    | false -> total
-
-                match Array.isEmpty pendingMigrations with
-                | true -> Array.empty
-                | false ->
-                    let amount =
-                        if total >= pendingMigrations.Length then
-                            pendingMigrations.Length - 1
-                        else
-                            total
-
-                    pendingMigrations.[..amount]
-            | _ -> pendingMigrations
-
-        match options.dryRun with
-        | true -> dryRunMigrations driver MigrationType.Up migrationsToRun
-        | false -> runMigrations driver connection MigrationType.Up migrationsToRun
-
-    let runMigrationsDown
-        (connection: IDbConnection)
-        (path: string, _: MigrondiConfig, driver: Driver)
-        (options: DownOptions)
-        =
-        let migration = getLastMigration connection
-
-        let migrations = getMigrations path
-
-        let alreadyRanMigrations =
-            getAppliedMigrations migration migrations
-            |> Array.rev
-
-        let amountToRunDown =
-            match options.total with
-            | number when number > 0 ->
-                match number > alreadyRanMigrations.Length with
-                | true ->
-                    printfn
-                        $"Total [{number}] provided exceeds the amount of migrations in the database ({alreadyRanMigrations.Length})"
-
-                    alreadyRanMigrations.Length
-                | false -> number
-            | _ -> alreadyRanMigrations.Length
-
-        match options.dryRun with
-        | true -> dryRunMigrations driver MigrationType.Down (alreadyRanMigrations |> Array.take amountToRunDown)
-        | false ->
-            runMigrations driver connection MigrationType.Down (alreadyRanMigrations |> Array.take amountToRunDown)
-
-    let runMigrationsList
-        (connection: IDbConnection)
-        (path: string, _: MigrondiConfig, _: Driver)
-        (options: ListOptions)
-        =
-
-        let migrations = getMigrations path
-        let migration = getLastMigration connection
-
-        match options.last, options.all, options.missing with
-        | (true, _, _) ->
-            match migration with
-            | Some migration ->
-                migrationName (MigrationSource.Database migration)
-                |> (fun name -> printfn $"Last migration in the database is {name}")
-
-            | None -> printfn "No migrations have been run in the database"
-        | (_, true, true) ->
             let pendingMigrations =
                 getPendingMigrations migration migrations
 
-            let migrations =
-                pendingMigrations
-                |> Array.map (
-                    MigrationSource.File
-                    >> migrationName
-                    >> sprintf "%s\n"
-                )
-                |> fun arr -> String.Join("", arr)
+            let getAmount amountToRun =
+                if amountToRun >= pendingMigrations.Length then
+                    pendingMigrations.Length - 1
+                else
+                    amountToRun
 
-            printfn $"Missing migrations:\n{migrations}"
-        | (_, true, false) ->
-            let alreadyRan =
+            let migrationsToRun =
+                if options.total > 0 then
+                    let total = getAmount options.total
+
+                    if Array.isEmpty pendingMigrations then
+                        Array.empty
+                    else
+                        pendingMigrations.[..total]
+                else
+                    pendingMigrations
+
+            if options.dryRun then
+                let migrations =
+                    dryRunMigrations driver MigrationType.Up migrationsToRun
+                // TODO: Print migration content
+                ()
+            else
+                let amount =
+                    runMigrations driver connection.Value MigrationType.Up migrationsToRun
+                    |> Array.length
+                // TODO: Print success message
+                ()
+
+            return 0
+        }
+
+    let tryRunMigrationsDown
+        (options: DownOptions)
+        (config: MigrondiConfig)
+        (getConnection: string -> Driver -> Lazy<IDbConnection>)
+        (getMigrations: TryGetMigrationsFn)
+        =
+        result {
+            let driver = Driver.FromString config.driver
+            let connection = getConnection config.connection driver
+
+            let! migration = getLastMigration connection.Value
+
+            let migrations = getMigrations config.migrationsDir
+
+            let alreadyRanMigrations =
                 getAppliedMigrations migration migrations
+                |> Array.rev
 
-            let migrations =
-                alreadyRan
-                |> Array.map (
-                    MigrationSource.File
-                    >> migrationName
-                    >> sprintf "%s\n"
-                )
-                |> fun arr -> String.Join("", arr)
+            let getAmountToRunDown amount =
+                result {
+                    if amount > 0 then
+                        if amount > alreadyRanMigrations.Length then
+                            let message =
+                                $"Total [{amount}] provided exceeds the amount of migrations in the database ({alreadyRanMigrations.Length})"
 
-            printfn $"Migrations that have been ran:\n{migrations}"
-        | (_, _, _) ->
-            printfn "This flag combination is not supported"
-            let l1 = "--last true"
-            let l2 = "--all true --missing true"
-            let l3 = "--all true --missing false"
-            printfn $"Suported comibations are:\n{l1}\n{l2}\n{l3}"
+                            return! Error(AmountExeedsExistingException message)
+
+                        return amount
+                    else
+                        return alreadyRanMigrations.Length
+                }
+
+            let! amountToRunDown = getAmountToRunDown options.total
+
+            if options.dryRun then
+                let migrations =
+                    dryRunMigrations driver MigrationType.Down (alreadyRanMigrations |> Array.take amountToRunDown)
+                // TODO: Print migration content
+                ()
+            else
+                let amount =
+                    runMigrations
+                        driver
+                        connection.Value
+                        MigrationType.Down
+                        (alreadyRanMigrations |> Array.take amountToRunDown)
+                // TODO: Print success message
+                ()
+
+            return 0
+        }
+
+    let tryRunMigrationsList
+        (options: ListOptions)
+        (config: MigrondiConfig)
+        (getConnection: string -> Driver -> Lazy<IDbConnection>)
+        (getMigrations: TryGetMigrationsFn)
+        =
+        result {
+            let driver = Driver.FromString config.driver
+            let connection = getConnection config.connection driver
+
+            let! migration = getLastMigration connection.Value
+
+            let migrations = getMigrations config.migrationsDir
+
+            match options.last, options.all, options.missing with
+            | (true, _, _) ->
+                match migration with
+                | Some migration ->
+                    migrationName (MigrationSource.Database migration)
+                    |> (fun name -> printfn $"Last migration in the database is {name}")
+
+                | None -> printfn "No migrations have been run in the database"
+            | (_, true, true) ->
+                let pendingMigrations =
+                    getPendingMigrations migration migrations
+
+                let migrations =
+                    pendingMigrations
+                    |> Array.map (
+                        MigrationSource.File
+                        >> migrationName
+                        >> sprintf "%s\n"
+                    )
+                    |> fun arr -> System.String.Join("", arr)
+
+                printfn $"Missing migrations:\n{migrations}"
+            | (_, true, false) ->
+                let alreadyRan =
+                    getAppliedMigrations migration migrations
+
+                let migrations =
+                    alreadyRan
+                    |> Array.map (
+                        MigrationSource.File
+                        >> migrationName
+                        >> sprintf "%s\n"
+                    )
+                    |> fun arr -> System.String.Join("", arr)
+
+                printfn $"Migrations that have been ran:\n{migrations}"
+            | (_, _, _) ->
+                printfn "This flag combination is not supported"
+                let l1 = "--last true"
+                let l2 = "--all true --missing true"
+                let l3 = "--all true --missing false"
+                printfn $"Suported comibations are:\n{l1}\n{l2}\n{l3}"
+
+            return 0
+        }
--- src/Migrondi/Program.fs
+++ src/Migrondi/Program.fs
@@ -1,185 +1,48 @@
 ﻿open CommandLine
-open Migrondi.Types
-open Migrondi.Options
-open Migrondi.Migrations
-open Migrondi.Utils
-open System.Data
+
 open FsToolkit.ErrorHandling
 
-
-let initializeDriver driver =
-    match driver with
-    | Driver.Mssql -> RepoDb.SqlServerBootstrap.Initialize()
-    | Driver.Sqlite -> RepoDb.SqLiteBootstrap.Initialize()
-    | Driver.Mysql -> RepoDb.MySqlBootstrap.Initialize()
-    | Driver.Postgresql -> RepoDb.PostgreSqlBootstrap.Initialize()
-
-let tryRunInit init : Result<int, exn> =
-    runMigrationsInit init
-    |> Result.map
-        (fun (filename, migrationsDir) ->
-            printfn $"Created: \"{filename}\" and \"{migrationsDir}\""
-            0)
-
-
-let tryRunNew (getConfig: unit -> Result<string * MigrondiConfig * Driver, exn>) newOptions =
-
-    try
-        match getConfig () with
-        | Ok config ->
-            runMigrationsNew config newOptions
-            Ok()
-        | Error ex -> Result.Error ex
-    with
-    | :? System.UnauthorizedAccessException ->
-        Result.Error(
-            exn "Failed to write migration to disk\nCheck that you have sufficient permission on the directory"
-        )
-
-let tryRunUp
-    (getConfig: unit -> Result<string * MigrondiConfig * Driver, exn>)
-    (getConnection: Driver -> string -> IDbConnection)
-    upOptions
-    =
-    try
-        match getConfig () with
-        | Ok (path, config, driver) ->
-            initializeDriver driver
-            let connection = getConnection driver config.connection
-
-            let result =
-                (runMigrationsUp connection (path, config, driver) upOptions)
-                    .Length
-
-            connection.Close()
-            Ok result
-
-        | Error err -> Result.Error err
-    with
-    | :? System.UnauthorizedAccessException as ex ->
-        let l1 = "Failed to write migration to disk"
-
-        let l2 =
-            "Check that you have sufficient permission on the directory"
-
-        let l3 = ex.Message
-        Result.Error(exn $"{l1}\n{l2}\n{l3}")
-    | ex -> Result.Error ex
-
-let tryRunDown
-    (getConfig: unit -> Result<string * MigrondiConfig * Driver, exn>)
-    (getConnection: Driver -> string -> IDbConnection)
-    downOptions
-    =
-    try
-        match getConfig () with
-        | Ok (path, config, driver) ->
-            initializeDriver driver
-            let connection = getConnection driver config.connection
-
-            let result =
-                (runMigrationsDown connection (path, config, driver) downOptions)
-                    .Length
-
-            connection.Close()
-            Ok result
-        | Error err -> Result.Error err
-    with
-    | ex -> Result.Error ex
-
-let tryRunList
-    (getConfig: unit -> Result<string * MigrondiConfig * Driver, exn>)
-    (getConnection: Driver -> string -> IDbConnection)
-    listOptions
-    =
-    try
-        match getConfig () with
-        | Ok (path, config, driver) ->
-            initializeDriver driver
-            let connection = getConnection driver config.connection
-            runMigrationsList connection (path, config, driver) listOptions
-            connection.Close()
-            Ok()
-        | Error err -> Result.Error err
-    with
-    | :? System.UnauthorizedAccessException ->
-        Result.Error(
-            exn "Failed to read migrations directory\nCheck that you have sufficient permission on the directory"
-        )
-    | ex -> Result.Error ex
-
-let tryGetConfig () =
-    try
-        Ok(getPathConfigAndDriver ())
-    with
-    | :? System.IO.FileNotFoundException
-    | :? System.ArgumentException
-    | :? System.ArgumentNullException
-    | :? System.IO.DirectoryNotFoundException
-    | :? System.IO.IOException
-    | :? System.IO.PathTooLongException
-    | :? System.NotSupportedException
-    | :? System.UnauthorizedAccessException as ex -> Result.Error ex
-    | :? System.Text.Json.JsonException ->
-        Result.Error(
-            exn
-                "Failed to parse the migrondi.json file, please check for trailing commas and that the properties have the correct name"
-        )
-
-
+open Migrondi.Types
+open Migrondi.FileSystem
+open Migrondi.Options
+open Migrondi.Migrations
+open Migrondi.Queries
 
 [<EntryPoint>]
 let main argv =
-    printfn "%s" System.Environment.CurrentDirectory
+    let parsedResult () =
+        match
+            CommandLine.Parser.Default.ParseArguments<InitOptions, NewOptions, UpOptions, DownOptions, ListOptions>
+                (argv)
+            with
+        | :? (Parsed<obj>) as command -> Ok command
+        | _ -> (Result.Error CommandNotParsedException)
 
-    let result =
-        CommandLine.Parser.Default.ParseArguments<InitOptions, NewOptions, UpOptions, DownOptions, ListOptions>(argv)
+    result {
 
-    match result with
-    | :? (NotParsed<obj>) -> 1
-    | :? (Parsed<obj>) as command ->
+        let! config = FileSystem.TryGetMigrondiConfig()
+
+        let! command = parsedResult ()
+
         match command.Value with
-        | :? NewOptions as newOptions ->
-            match tryRunNew tryGetConfig newOptions with
-            | Ok _ -> 0
-            | Error err ->
-                printfn $"{err.Message}"
-                1
-        | :? UpOptions as upOptions ->
-            match tryRunUp tryGetConfig getConnection upOptions with
-            | Ok amount ->
-                match upOptions.dryRun with
-                | true -> printfn $"Total Migrations To Run: %i{amount}"
-                | false -> printfn $"Migrations Applied: %i{amount}"
-
-                0
-            | Error err ->
-                printfn $"{err.Message}"
-                1
-        | :? DownOptions as downOptions ->
-            match tryRunDown tryGetConfig getConnection downOptions with
-            | Ok amount ->
-                match downOptions.dryRun with
-                | true -> printfn $"Total Migrations To Run: %i{amount}"
-                | false -> printfn $"Rolled back %i{amount} migrations"
-
-                0
-            | Error err ->
-                printfn $"{err.Message}"
-                1
-        | :? ListOptions as listOptions ->
-            match tryRunList tryGetConfig getConnection listOptions with
-            | Ok _ -> 0
-            | Error err ->
-                printfn $"{err.Message}"
-                1
         | :? InitOptions as initOptions ->
-            tryRunInit initOptions
-            |> Result.mapError Spectre.Console.AnsiConsole.WriteException
-            |> Result.defaultValue 0
-        | _ ->
-            printfn "Unexpected parsing result"
+            return!
+                tryRunMigrationsInit
+                    FileSystem.TryGetOrCreateDirectory
+                    FileSystem.TryGetOrCreateConfiguration
+                    initOptions
+        | :? NewOptions as newOptions ->
+            return! tryRunMigrationsNew FileSystem.TryCreateNewMigrationFile config newOptions
+        | :? UpOptions as upOptions ->
+            return! tryRunMigrationsUp upOptions config getConnection FileSystem.GetMigrations
+        | :? DownOptions as downOptions ->
+            return! tryRunMigrationsDown downOptions config getConnection FileSystem.GetMigrations
+        | :? ListOptions as listOptions ->
+            return! tryRunMigrationsList listOptions config getConnection FileSystem.GetMigrations
+        | parsed -> return! (Result.Error(InvalidOptionSetException(nameof parsed)))
+    }
+    |> function
+        | Ok exitCode -> exitCode
+        | Error ex ->
+            Spectre.Console.AnsiConsole.WriteException ex
             1
-    | _ ->
-        printfn "Unexpected parsing result"
-        1
