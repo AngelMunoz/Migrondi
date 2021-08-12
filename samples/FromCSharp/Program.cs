5ac3114c8d5ec8010b9c9bf1b6816d475b74701a
Angel D. Munoz <scythes.lol@gmail.com>
2021-08-11 23:16:58 -0500

    register viewmodels in assembly

--- samples/FromCSharp/Program.cs
+++ samples/FromCSharp/Program.cs
@@ -1,8 +1,11 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
-
 using System;
+using System.Reflection;
+using FromCSharp.ViewModels;
+using ReactiveUI;
+using Splat;
 
 namespace FromCSharp
 {
@@ -16,9 +19,12 @@ namespace FromCSharp
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
+        {
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+            return AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .LogToTrace()
                 .UseReactiveUI();
+        }
     }
 }
