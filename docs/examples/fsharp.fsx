d8bd9716ed58f2f41b0f8d4a836fa8645b5673d9
Angel D. Munoz <scythes.lol@gmail.com>
2023-10-02 17:20:33 -0600

    update example

--- docs/examples/fsharp.fsx
+++ docs/examples/fsharp.fsx
@@ -1,10 +1,9 @@
+(**
 ---
 title: F# Scripts
 category: Examples
-categoryIndex: 0
-index: 1
+categoryindex: 1
 ---
-
 Migrondi can be used in F# scripts, which is a great way to get the hang of the library and easy to prototype.
 
 The following example shows a couple of things
@@ -14,7 +13,10 @@ The following example shows a couple of things
 - Create a new migration
 - Do a dry run of the migrations
 
-```fsharp
+We'll start with a little prelude of the code that is needed to get the dependencies for the services.
+and opening the required namespaces.
+*)
+
 #r "nuget: Microsoft.Extensions.Logging"
 #r "nuget: Microsoft.Extensions.Logging.Console"
 #r "nuget: Migrondi.Core, 1.0.0-beta-003"
@@ -32,7 +34,9 @@ open Microsoft.Extensions.Logging
 module Config =
   let getLogger loggerName =
     let loggerFactory =
-      LoggerFactory.Create(fun builder -> builder.SetMinimumLevel(LogLevel.Debug).AddSimpleConsole() |> ignore)
+      LoggerFactory.Create(fun builder ->
+        builder.SetMinimumLevel(LogLevel.Debug).AddSimpleConsole() |> ignore
+      )
 
     loggerFactory.CreateLogger loggerName
 
@@ -46,13 +50,17 @@ module Config =
     else
       $"{migrationsDir}{Path.DirectorySeparatorChar}"
 
-// Get a serializer instance this works for both configuration and SQL files
-let serializer = SerializerServiceFactory.GetInstance()
+(**
+We need to get a serializer, a logger, and a configuration object to create the services.
+mostly because this is designed to play well with DI containers.
 
+
+ *)
 let config = MigrondiConfig.Default
+
 let logger = Config.getLogger("sample-app")
 
-// once we have a serializer, a logger, and a configuration object we can start working with the rest of the library
+let serializer = SerializerServiceFactory.GetInstance()
 
 let fileSystemService =
   let migrationsDir = Config.ensureWellFormed config.migrations
@@ -60,13 +68,14 @@ let fileSystemService =
   FileSystemServiceFactory.GetInstance(
     serializer,
     logger,
-    Uri(__SOURCE_DIRECTORY__ + $"{Path.DirectorySeparatorChar}", UriKind.Absolute),
+    Uri(
+      __SOURCE_DIRECTORY__ + $"{Path.DirectorySeparatorChar}",
+      UriKind.Absolute
+    ),
     Uri(migrationsDir, UriKind.Relative)
   )
 
 let databaseService = DatabaseServiceFactory.GetInstance(logger, config)
-
-databaseService.SetupDatabase()
 
 let migrondi =
   MigrondiServiceFactory.GetInstance(
@@ -76,7 +85,23 @@ let migrondi =
     config
   )
 
-// Let's create a new Migration
+(**
+For any database related work you _MUST_ call `SetupDatabase` before doing anything else.
+Otherwise the driver won't be initialized and the tables won't be created.
+*)
+
+
+databaseService.SetupDatabase()
+
+(**
+We can start with a simple migration.
+
+> The name schema for the migrations is basically a string separated by an underscode and a timestamp followed by the .sql extension
+
+> ```fsharp
+> let MigrationNameSchema = "(.+)_([0-9]+).(sql|SQL)"
+> ```
+*)
 
 let timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
 let name = $"add-test-table_{timestamp}.sql"
@@ -91,15 +116,25 @@ fileSystemService.WriteMigration(
   name
 )
 
-// get the list of migrations without actually running them
+
+(**
+Once the services are in place and we have a migration in the local file system then we can
+Attempt a dry run of the migrations.
+
+Dry runs only return the scripts that would be executed in the order they would be executed.
+so you can `for migration in migrations do ...` and review that everything comes in order.
+*)
+
 let applied = migrondi.DryRunUp()
 
-logger.LogInformation($"List of the migrations that would have been ran: %A{applied}")
-```
+logger.LogInformation(
+  $"List of the migrations that would have been ran: %A{applied}"
+)
 
+(**
 That should show you a list of the migrations that would have been ran.
 
-```
+```text
 dbug: sample-app[0]
       Writing migration to c:\path\to\scripts\migrations\add-test-table_1696144424109.sql with directory: c:\path\to\scripts\migrations\ and name: add-test-table_1696144424109.sql
 dbug: sample-app[0]
@@ -110,3 +145,7 @@ info: sample-app[0]
    upContent = "create table if not exists test (id int not null primary key);"
    downContent = "drop table if exists test;" }]
 ```
+
+Keep in mind that the migrations are not actually ran, so the database is not updated.
+From there on you can encode your logic specifically to know when to apply or revert migrations.
+*)
