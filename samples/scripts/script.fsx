ac1b97637884cfb37f041758cede6b521c35ebd2
Angel D. Munoz <scythes.lol@gmail.com>
2024-02-08 10:28:00 -0600

    Fix sample with missing api

--- samples/scripts/script.fsx
+++ samples/scripts/script.fsx
@@ -1,21 +1,19 @@
 #r "nuget: Microsoft.Extensions.Logging"
 #r "nuget: Microsoft.Extensions.Logging.Console"
-#r "nuget: Migrondi.Core, 1.0.0-beta-003"
+#r "nuget: Migrondi.Core, 1.0.0-beta-007"
 
 open System
 open System.IO
 open Migrondi.Core
-open Migrondi.Core.Database
-open Migrondi.Core.FileSystem
-open Migrondi.Core.Migrondi
-open Migrondi.Core.Serialization
 
 open Microsoft.Extensions.Logging
 
 module Config =
   let getLogger loggerName =
     let loggerFactory =
-      LoggerFactory.Create(fun builder -> builder.SetMinimumLevel(LogLevel.Debug).AddSimpleConsole() |> ignore)
+      LoggerFactory.Create(fun builder ->
+        builder.SetMinimumLevel(LogLevel.Debug).AddSimpleConsole() |> ignore
+      )
 
     loggerFactory.CreateLogger loggerName
 
@@ -29,51 +27,33 @@ module Config =
     else
       $"{migrationsDir}{Path.DirectorySeparatorChar}"
 
-// Get a serializer instance this works for both configuration and SQL files
-let serializer = SerializerServiceFactory.GetInstance()
 
 let config = MigrondiConfig.Default
 let logger = Config.getLogger("sample-app")
 
-// once we have a serializer, a logger, and a configuration object we can start working with the rest of the library
+let factory = Migrondi.MigrondiFactory(logger)
+let migrationsDir = Config.ensureWellFormed config.migrations
 
-let fileSystemService =
-  let migrationsDir = Config.ensureWellFormed config.migrations
-
-  FileSystemServiceFactory.GetInstance(
-    serializer,
-    logger,
-    Uri(__SOURCE_DIRECTORY__ + $"{Path.DirectorySeparatorChar}", UriKind.Absolute),
+let migrondi =
+  factory.Invoke(
+    config,
+    Uri(
+      __SOURCE_DIRECTORY__ + $"{Path.DirectorySeparatorChar}",
+      UriKind.Absolute
+    ),
     Uri(migrationsDir, UriKind.Relative)
   )
 
-let databaseService = DatabaseServiceFactory.GetInstance(logger, config)
-
-databaseService.SetupDatabase()
-
-let migrondi =
-  MigrondiServiceFactory.GetInstance(
-    databaseService,
-    fileSystemService,
-    logger,
-    config
-  )
-
 // Let's create a new Migration
-
-let timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
-let name = $"add-test-table_{timestamp}.sql"
-
-fileSystemService.WriteMigration(
-  {
-    name = name
-    timestamp = timestamp
-    upContent = "create table if not exists test (id int not null primary key);"
-    downContent = "drop table if exists test;"
-  },
-  name
+migrondi.RunNew(
+  "add-test-table",
+  "create table if not exists test (id int not null primary key);",
+  "drop table if exists test;"
 )
+
 
 let applied = migrondi.DryRunUp()
 
-logger.LogInformation($"List of the migrations that would have been ran: %A{applied}")
+logger.LogInformation(
+  $"List of the migrations that would have been ran: %A{applied}"
+)
