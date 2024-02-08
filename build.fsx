3d2866ab9162556e12b62674f117d4c39c73f55a
Angel D. Munoz <scythes.lol@gmail.com>
2024-02-08 10:28:11 -0600

    bump version

--- build.fsx
+++ build.fsx
@@ -23,7 +23,7 @@ let libraries = [ "Migrondi.Core" ]
 let NugetApiKey = EnvVar.getOrFail "NUGET_DEPLOY_KEY"
 
 [<Literal>]
-let PackageVersion = "1.0.0-beta-006"
+let PackageVersion = "1.0.0-beta-007"
 
 let fsSources =
   Glob.create "*.fsx"
