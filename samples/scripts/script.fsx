0eee8052ce7fdf42e61144c730a3f871e10d58ae
Angel D. Munoz <scythes.lol@gmail.com>
2024-02-17 20:17:45 -0600

    update sample script

--- samples/scripts/script.fsx
+++ samples/scripts/script.fsx
@@ -1,49 +1,12 @@
-#r "nuget: Microsoft.Extensions.Logging"
-#r "nuget: Microsoft.Extensions.Logging.Console"
-#r "nuget: Migrondi.Core, 1.0.0-beta-008"
+#r "nuget: Migrondi.Core, 1.0.0-beta-009"
 
 open System
 open System.IO
 open Migrondi.Core
 
-open Microsoft.Extensions.Logging
-
-module Config =
-  let getLogger loggerName =
-    let loggerFactory =
-      LoggerFactory.Create(fun builder ->
-        builder.SetMinimumLevel(LogLevel.Debug).AddSimpleConsole() |> ignore
-      )
-
-    loggerFactory.CreateLogger loggerName
-
-  let ensureWellFormed (migrationsDir: string) =
-    // when you're using URIs "./path" is not the same as "./path/"
-    // the first one is a file and the second one is a directory
-    // so ensure you always end your paths with a directory separator
-
-    if Path.EndsInDirectorySeparator(migrationsDir) then
-      migrationsDir
-    else
-      $"{migrationsDir}{Path.DirectorySeparatorChar}"
-
-
 let config = MigrondiConfig.Default
-let logger = Config.getLogger("sample-app")
 
-let factory = Migrondi.MigrondiFactory(logger)
-let migrationsDir = Config.ensureWellFormed config.migrations
-
-let migrondi =
-  factory.Invoke(
-    config,
-    Uri(
-      __SOURCE_DIRECTORY__ + $"{Path.DirectorySeparatorChar}",
-      UriKind.Absolute
-    ),
-    Uri(migrationsDir, UriKind.Relative)
-  )
-
+let migrondi = Migrondi.MigrondiFactory(config, ".")
 
 // Let's create a new Migration, since this is just an I/O operation
 // there's no need to initialize the database yet, but ideally
@@ -54,12 +17,14 @@ migrondi.RunNew(
   "drop table if exists test;"
 )
 
-// Before we can do anything we need to initialize the migrondi service
-// this will ensure that the migrations directory exists and that the
-// database is ready to accept migrations
+// Before we can talk to the database we need to initialize the migrondi object
+// this will ensure that the migrations directory exists, and the required pieces
+// of information are available in the database to see if it is ready to accept migrations.
 migrondi.Initialize()
+
+// once that the migrondi service is initialized we can try to commmunicate to the
+// database and in this case go for a dry run
 let applied = migrondi.DryRunUp()
 
-logger.LogInformation(
-  $"List of the migrations that would have been ran: %A{applied}"
-)
+printfn
+  $"List of the migrations that would have been ran:\n\n%A{applied}"
