c60e4526f3433c59b51ab895d8e2ad73b0cfc6ee
Angel D. Munoz <scythes.lol@gmail.com>
2025-05-01 21:34:30 -0600

    feat: refactor Landing view and routing to improve project selection handling

--- src/MigrondiUI/Views/Landing.fs
+++ src/MigrondiUI/Views/Landing.fs
@@ -1,30 +1,99 @@
 namespace MigrondiUI.Views
 
+open System
+
+open IcedTasks
+open IcedTasks.Polyfill.Async.PolyfillBuilders
+
 open Avalonia.Controls
+
 open NXUI.Extensions
 
+open FSharp.Data.Adaptive
+
+open Navs
 open Navs.Avalonia
+open MigrondiUI
+open MigrondiUI.Projects
 
 module Landing =
-  let View _ _ =
-    UserControl()
-      .Tag("Landing")
-      .Content(
-        StackPanel()
-          .Children(
-            TextBlock().Text("Welcome to Migrondi!"),
-            Button()
-              .Content("Create a new project")
-              .OnClickHandler(fun _ _ -> printfn "Create a new project!"),
-            Button()
-              .Content("Open an existing project")
-              .OnClickHandler(fun _ _ -> printfn "Open an existing project!"),
-            Button()
-              .Content("Settings")
-              .OnClickHandler(fun _ _ -> printfn "Open settings!"),
-            Button().Content("Exit").OnClickHandler(fun _ _ -> printfn "Exit!")
-          )
-          .Spacing(10)
-          .Margin(10)
-          .OrientationVertical()
+  open Avalonia.Controls.Templates
+  open System.Collections.Generic
+
+  let inline localProjectTemplate(project: LocalProject) =
+
+    StackPanel()
+      .Children(
+        TextBlock().Text(project.name),
+        TextBlock()
+          .Text(project.description |> Option.defaultValue "No description")
       )
+      .Spacing(5)
+      .Margin(5)
+      .OrientationVertical()
+
+  let repositoryItem =
+    FuncDataTemplate<Project>(fun project _ ->
+      StackPanel()
+        .Tag(project.Id)
+        .Children(
+          TextBlock().Text project.Name,
+          TextBlock().Text(defaultArg project.Description "No description")
+        )
+        .Spacing(5)
+        .Margin(5)
+        .OrientationVertical())
+
+  let repositoryList onProjectSelected (projects: Project list aval) =
+    let lb =
+      ListBox()
+        .ItemsSource(projects |> AVal.toBinding)
+        .ItemTemplate(repositoryItem)
+        .OnSelectionChangedHandler(fun _ args ->
+          if args.AddedItems.Count > 0 then
+            match args.AddedItems.Item 0 with
+            | :? Project as project -> onProjectSelected project
+            | _ -> ()
+          else
+            ())
+
+    lb.SelectionMode <- SelectionMode.Single
+    ScrollViewer().Name("ProjectList").Content(lb)
+
+  type LandingVM(projects: IProjectRepository) =
+
+    let _projects = cval []
+    member this.Projects: Project list aval = _projects
+
+    member _.LoadProjects() = async {
+      let! projects = projects.GetProjects()
+      _projects.setValue projects
+      return ()
+    }
+
+  let View (vm: LandingVM) _ (nav: INavigable<Control>) : Async<Control> = async {
+    vm.LoadProjects() |> Async.StartImmediate
+
+    let handleProjectSelected(project: Project) =
+      nav.Navigate $"/projects/%s{project.Id.ToString()}"
+      |> Async.AwaitTask
+      |> Async.Ignore
+      |> Async.StartImmediate
+      // Handle the selected project here
+      printfn $"Selected project: %s{project.Name}"
+
+    return
+      UserControl()
+        .Name("Landing")
+        .Content(
+          StackPanel()
+            .Children(
+              TextBlock().Text("Welcome to Migrondi!"),
+              repositoryList handleProjectSelected vm.Projects
+            )
+            .Spacing(10)
+            .Margin(10)
+            .OrientationVertical()
+        )
+      :> Control
+  }
--- src/MigrondiUI/Views/Routes.fs
+++ src/MigrondiUI/Views/Routes.fs
@@ -8,9 +8,16 @@ open Navs.Avalonia
 module Routes =
   open MigrondiUI.Projects
 
-  let GetRouter(_: IProjectRepository) =
+  /// Factory function to create a Landing view and its VM from an IProjectRepository
+  let private landingViewWithVM projects =
+    Landing.View(Landing.LandingVM projects)
+
+  let GetRouter(projects: IProjectRepository) =
+
     let router: IRouter<_> =
-      AvaloniaRouter [ Route.define("landing", "/", Landing.View) ]
+      AvaloniaRouter [
+        Route.define("landing", "/", landingViewWithVM projects)
+      ]
 
     router.Navigate("/")
     |> Async.AwaitTask
