221fd7d818f0dd22d8e7d9e240d6035cdea8cc0a
Angel D. Munoz <scythes.lol@gmail.com>
2025-05-02 18:13:16 -0600

    feat: add extension methods for ListBox and SelectingItemsControl

--- src/MigrondiUI/Extensions.fs
+++ src/MigrondiUI/Extensions.fs
@@ -1,2 +1,19 @@
 [<AutoOpen>]
 module Extensions
+
+open Avalonia.Controls
+open Avalonia.Controls.Primitives
+
+open NXUI.Extensions
+
+
+type ListBox with
+  member inline this.SelectionMode(mode: SelectionMode) =
+    this.SelectionMode <- mode
+    this
+
+type SelectingItemsControl with
+
+  member inline this.OnSelectionChanged<'T>(f: 'T seq * 'T seq -> unit) =
+    this.OnSelectionChangedHandler(fun _ args ->
+      f(args.AddedItems |> Seq.cast<'T>, args.RemovedItems |> Seq.cast<'T>))
