77a152472512fca7ae23d0c20746a8e03e143fc9
Angel D. Munoz <scythes.lol@gmail.com>
2025-05-05 13:06:46 -0600

    feat(views): enhance LocalProjectDetails with migration management - add ListMigrations method to LocalProjectDetailsVM for async migration listing - implement migrationView for displaying migration details - create migrationsPanel to show migration status or a message if none found - update View method to initialize migrations on load

--- src/MigrondiUI/Views/ProjectDetails.fs
+++ src/MigrondiUI/Views/ProjectDetails.fs
@@ -1,6 +1,8 @@
 namespace MigrondiUI.Views
 
 open System
+open System.IO
+open Microsoft.Extensions.Logging
 open Avalonia.Controls
 open NXUI.Extensions
 open FSharp.Data.Adaptive
@@ -9,13 +11,14 @@ open IcedTasks.Polyfill.Async.PolyfillBuilders
 open FsToolkit.ErrorHandling
 open Navs
 open Navs.Avalonia
+open Migrondi.Core
 open MigrondiUI
 open MigrondiUI.Projects
 
 module LocalProjectDetails =
-  open Microsoft.Extensions.Logging
-  open Migrondi.Core
-  open System.IO
+  open Avalonia.Controls.Templates
+  open Avalonia.Media
+
 
   type LocalProjectDetailsVM
     (
@@ -24,11 +27,85 @@ module LocalProjectDetails =
       project: LocalProject
     ) =
 
-    do logger.LogDebug "LocalProjectDetailsVM created"
+    let _migrations = cval [||]
 
+    do
+      logger.LogDebug "LocalProjectDetailsVM created"
+      migrondi.Initialize()
 
     member _.Project = project
 
+    member _.Migrations: MigrationStatus[] aval = _migrations
+
+    member _.ListMigrations() = async {
+      logger.LogDebug "Listing migrations"
+      let! token = Async.CancellationToken
+      let! migrations = migrondi.MigrationsListAsync(token)
+
+      logger.LogDebug("Migrations listed: {migrations}", migrations.Count)
+
+      _migrations.setValue [| yield! migrations |]
+
+      return ()
+    }
+
+  let migrationView =
+    FuncDataTemplate<MigrationStatus>(fun migrationStatus _ ->
+      let migration =
+        match migrationStatus with
+        | Applied m -> m
+        | Pending m -> m
+
+      let status =
+        match migrationStatus with
+        | Applied _ -> "Applied"
+        | Pending _ -> "Pending"
+
+      let strDate =
+        DateTimeOffset.FromUnixTimeMilliseconds migration.timestamp
+        |> _.ToString("R")
+
+      let migrationContent =
+        Grid()
+          .ColumnDefinitions("*,4,*")
+          .Children(
+            LabeledField.Vertical("Migrate Up", migration.upContent).Column(0),
+            GridSplitter().Column(1).ResizeDirectionColumns(),
+            LabeledField
+              .Vertical("Migrate Down", migration.downContent)
+              .Column(2)
+          )
+
+      Expander()
+        .Header(
+          StackPanel()
+            .Children(
+              TextBlock().Text(migration.name),
+              TextBlock()
+                .Text($" [{status}]")
+                .Foreground(
+                  match migrationStatus with
+                  | Applied _ -> "Green"
+                  | Pending _ -> "OrangeRed"
+                  |> SolidColorBrush.Parse
+                ),
+              TextBlock().Text($" - {strDate}")
+            )
+            .OrientationHorizontal()
+        )
+        .Content(
+          StackPanel()
+            .Children(
+              LabeledField.Horizontal(
+                "Manual Transaction:",
+                $" %b{migration.manualTransaction}"
+              ),
+              ScrollViewer().Content migrationContent
+            )
+            .Spacing(8)
+        )
+        .HorizontalAlignmentStretch()
+        .VerticalAlignmentStretch())
 
   let toolbar(onNavigateBack: unit -> unit) : Control =
     StackPanel()
@@ -72,6 +149,17 @@ module LocalProjectDetails =
       .Header($"{project.name} - {description}")
       .Content(configView(project.migrondiConfigPath, config))
 
+  let migrationsPanel(migrations: aval<MigrationStatus[]>) : Control =
+
+    let migrationListView =
+      migrations
+      |> AVal.map(fun migrations ->
+        if migrations.Length = 0 then
+          TextBlock().Text "No migrations found." :> Control
+        else
+          ItemsControl().ItemsSource(migrations).ItemTemplate(migrationView))
+
+    ScrollViewer().Content(migrationListView |> AVal.toBinding)
 
   let View
     (logger: ILogger<LocalProjectDetailsVM>, projects: IProjectRepository)
@@ -107,8 +195,10 @@ module LocalProjectDetails =
 
         let! config = project.config
 
-        let migrondi =
-          Migrondi.MigrondiFactory(config, project.migrondiConfigPath)
+        let projectRoot =
+          Path.GetDirectoryName project.migrondiConfigPath |> nonNull
+
+        let migrondi = Migrondi.MigrondiFactory(config, projectRoot)
 
         return LocalProjectDetailsVM(logger, migrondi, project)
       }
@@ -125,13 +215,15 @@ module LocalProjectDetails =
 
       match! vm with
       | Some vm ->
+        vm.ListMigrations() |> Async.StartImmediate
+
         return
           UserControl()
             .Name("ProjectDetails")
             .Content(
               Grid()
-                .RowDefinitions("5*,95*")
-                .ColumnDefinitions("Auto,*")
+                .RowDefinitions("Auto,Auto,*")
+                .ColumnDefinitions("Auto,*,*")
                 .Children(
                   toolbar(onNavigateBack)
                     .Row(0)
@@ -141,8 +233,15 @@ module LocalProjectDetails =
                   localProjectView(vm.Project)
                     .Row(1)
                     .Column(0)
-                    .ColumnSpan(2)
+                    .ColumnSpan(3)
                     .VerticalAlignmentTop()
+                    .HorizontalAlignmentStretch()
+                    .MarginY(8),
+                  migrationsPanel(vm.Migrations)
+                    .Row(2)
+                    .Column(0)
+                    .ColumnSpan(3)
+                    .VerticalAlignmentStretch()
                     .HorizontalAlignmentStretch()
                     .MarginY(8)
                 )
