51e84e1f5084fa41c547c8df70435534b6ec1d56
Angel D. Munoz <scythes.lol@gmail.com>
2025-06-01 10:01:22 -0600

    feat(ui): add VirtualProjectForm and MigrationRunnerToolbar components
    - Implement VirtualProjectForm for creating and editing virtual projects.
    - Add MigrationRunnerToolbar to manage migration operations including applying and rolling back migrations.
    - Update MigrondiUI.fsproj to include new components.

---
+++ src/MigrondiUI/Components/MigrationRunnerToolbar.fs
@@ -0,0 +1,116 @@
+﻿module MigrondiUI.Components.MigrationRunnerToolbar
+
+open System
+
+open Avalonia.Controls
+open FSharp.Data.Adaptive
+open NXUI.Extensions
+open Navs.Avalonia
+open MigrondiUI.Components.ProjectDetails
+
+let applyPendingButton (dryRun, onRunMigrationsRequested, getIntValue) =
+  let btn =
+    dryRun
+    |> AVal.map(fun dryRun ->
+      if dryRun then
+        Button()
+          .Content("Apply Pending (Dry Run)")
+          .OnClickHandler(fun _ _ ->
+            onRunMigrationsRequested(RunMigrationKind.DryUp, getIntValue()))
+        :> Control
+      else
+        SplitButton()
+          .Content("Apply Pending")
+          .Flyout(
+            Flyout()
+              .Content(
+                Button()
+                  .Content("Confirm Apply")
+                  .OnClickHandler(fun _ _ ->
+                    onRunMigrationsRequested(
+                      RunMigrationKind.Up,
+                      getIntValue()
+                    ))
+              )
+          ))
+
+  UserControl().Name("ApplyPendingButton").Content(btn |> AVal.toBinding)
+
+let rollbackButton (dryRun, onRunMigrationsRequested, getIntValue) =
+  let btn =
+    dryRun
+    |> AVal.map(fun dryRun ->
+      if dryRun then
+        Button()
+          .Content("Rollback (Dry Run)")
+          .OnClickHandler(fun _ _ ->
+            onRunMigrationsRequested(
+              RunMigrationKind.DryDown,
+              getIntValue()
+            ))
+        :> Control
+      else
+        SplitButton()
+          .Content("Rollback")
+          .Flyout(
+            Flyout()
+              .Content(
+                Button()
+                  .Content("Confirm Rollback")
+                  .OnClickHandler(fun _ _ ->
+                    onRunMigrationsRequested(
+                      RunMigrationKind.Down,
+                      getIntValue()
+                    ))
+              )
+          ))
+
+  UserControl().Name("RollbackButton").Content(btn |> AVal.toBinding)
+
+let numericUpDown (steps: _ cval) =
+  NumericUpDown()
+    .Minimum(0)
+    .Value(steps |> AVal.toBinding)
+    .Watermark("Amount to run")
+    .OnValueChangedHandler(fun _ value ->
+      match value.NewValue |> ValueOption.ofNullable with
+      | ValueNone -> steps.setValue 1M
+      | ValueSome value -> steps.setValue value)
+
+let checkBox (dryRun: _ cval) =
+    CheckBox()
+      .Content("Dry Run")
+      .IsChecked(dryRun |> AVal.toBinding)
+      .OnIsCheckedChangedHandler(fun checkbox _ ->
+
+        let isChecked =
+          checkbox.IsChecked
+          |> ValueOption.ofNullable
+          |> ValueOption.defaultValue true
+
+        dryRun.setValue isChecked)
+
+
+
+type MigrationsRunnerToolbar(onRunMigrationsRequested: RunMigrationKind * int -> unit) =
+  inherit UserControl()
+  let dryRun = cval false
+  let steps = cval 1M
+
+  let getIntValue() =
+    try
+      let v = steps.getValue() |> int
+      if v < 0 then 1 else v
+    with :? OverflowException ->
+      1
+  do
+    base.Content <-
+      StackPanel()
+        .OrientationHorizontal()
+        .Spacing(8)
+        .Children(
+          applyPendingButton(dryRun, onRunMigrationsRequested, getIntValue),
+          rollbackButton(dryRun, onRunMigrationsRequested, getIntValue),
+          checkBox(dryRun),
+          numericUpDown(steps)
+        )
---
+++ src/MigrondiUI/Components/VirtualProjectForm.fs
@@ -0,0 +1,115 @@
+﻿module MigrondiUI.Components.VirtualProjectForm
+
+open System
+open Avalonia.Controls
+open NXUI.Extensions
+open FSharp.Data.Adaptive
+open IcedTasks
+open FsToolkit.ErrorHandling
+open Migrondi.Core
+open Navs.Avalonia
+open MigrondiUI
+open MigrondiUI.Components.Fields
+
+module VirtualProject =
+
+  let setName (name: string) (project: VirtualProject cval) =
+    project.setValue(fun p -> { p with name = name })
+
+  let setDescription
+    (description: string | null)
+    (project: VirtualProject cval)
+    =
+    let description =
+      if String.IsNullOrWhiteSpace(description) then
+        None
+      else
+        Some(nonNull description)
+
+    project.setValue(fun p -> { p with description = description })
+
+  let setConnection (connection: string | null) (project: VirtualProject cval) =
+    Ok connection
+    |> Result.bind(fun conn ->
+      if String.IsNullOrWhiteSpace(conn) then
+        Error "Connection string cannot be empty."
+      else
+        Ok(nonNull conn))
+    |> Result.toOption
+    |> Option.iter(fun connection ->
+      project.setValue(fun p -> { p with connection = connection }))
+
+  let setDriver (driver: string) (project: VirtualProject cval) =
+    project.setValue(fun p -> {
+      p with
+          driver = MigrondiDriver.FromString driver
+    })
+
+
+type VirtualProjectForm
+  (project: VirtualProject, onSave: VirtualProject -> Async<unit>) =
+  inherit UserControl()
+
+  let aProject = cval project
+
+  let driverItems = [|
+    MigrondiDriver.Mysql.AsString
+    MigrondiDriver.Postgresql.AsString
+    MigrondiDriver.Mssql.AsString
+    MigrondiDriver.Sqlite.AsString
+  |]
+
+  let saveBtn =
+    let isSaving = cval false
+
+    Button()
+      .Content("Save Project")
+      .IsEnabled(isSaving |> AVal.map not |> AVal.toBinding)
+      .OnClickHandler(fun _ _ ->
+        asyncEx {
+          isSaving.setValue true
+          let project = AVal.force aProject
+          do! onSave project
+          isSaving.setValue false
+        }
+        |> Async.StartImmediate)
+
+  do
+    base.Content <-
+      StackPanel()
+        .Spacing(8)
+        .Children(
+          LabeledField.Vertical("Project Name", project.name),
+          LabeledField.Vertical(
+            "Description",
+            TextBox()
+              .AcceptsReturn(true)
+              .Height(60.0)
+              .Text(
+                aProject
+                |> AVal.map(fun p -> p.description |> Option.defaultValue "")
+                |> AVal.toBinding
+              )
+              .OnTextChangedHandler(fun tb _ ->
+                aProject |> VirtualProject.setDescription(tb.Text))
+          ),
+          LabeledField.Vertical(
+            "Connection String",
+            TextBox()
+              .Text(aProject |> AVal.map(_.connection) |> AVal.toBinding)
+              .OnTextChangedHandler(fun tb _ ->
+                aProject |> VirtualProject.setConnection(tb.Text))
+          ),
+          LabeledField.Vertical(
+            "Database Driver",
+            ComboBox()
+              .ItemsSource(driverItems)
+              .OnSelectionChanged<string>(fun ((selected, _), _) ->
+                selected
+                |> Seq.tryHead
+                |> Option.iter(fun s ->
+                  aProject |> VirtualProject.setDriver(s)))
+          ),
+
+          saveBtn
+        )
