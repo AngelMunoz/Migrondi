220a9c2108b449818f9ab76ef88536a0c69386bb
Angel D. Munoz <scythes.lol@gmail.com>
2025-06-05 12:10:00 -0600

    feat(fs): implement ImportFromLocal and ExportToLocal

--- src/MigrondiUI/VirtualFs.fs
+++ src/MigrondiUI/VirtualFs.fs
@@ -2,9 +2,9 @@ module MigrondiUI.VirtualFs
 
 open System
 open System.Collections.Generic
+open System.IO
 open System.Text.RegularExpressions
 open System.Threading
-open System.Threading.Tasks
 open IcedTasks
 
 open Migrondi.Core
@@ -13,6 +13,7 @@ open Migrondi.Core.Serialization
 
 open MigrondiUI.Projects
 open Microsoft.Extensions.Logging
+open FsToolkit.ErrorHandling
 
 [<Literal>]
 let MigrationNameSchema: string = "^(?<Timestamp>[0-9]+)_(?<Name>.+).(sql|SQL)$"
@@ -45,7 +46,8 @@ let extractTimestampAndName(name: string) =
 type MigrondiUIFs =
   inherit IMiFileSystem
 
-  abstract member ExportToLocal: project: Guid * projectPath: string -> CancellableTask<string>
+  abstract member ExportToLocal:
+    project: Guid * projectPath: string -> CancellableTask<string>
 
   abstract member ImportFromLocal:
     project: LocalProject -> CancellableTask<Guid>
@@ -55,6 +57,91 @@ type MigrondiUIFs =
 let getVirtualFs
   (logger: ILogger<MigrondiUIFs>, vpr: IVirtualProjectRepository)
   =
+  let importProjectFromPath (configPath: string) (projectName: string) = cancellableTask {
+    let configSerializer, migrationSerializer =
+      let serializer = MigrondiSerializer()
+
+      serializer :> IMiConfigurationSerializer,
+      serializer :> IMiMigrationSerializer
+
+    let rootDir = Path.GetDirectoryName(configPath)
+
+    logger.LogInformation(
+      "Importing project {projectName} from {rootDir}",
+      projectName,
+      rootDir
+
+    )
+
+    let! config =
+      (fun token -> File.ReadAllTextAsync(configPath, token))
+      |> CancellableTask.map(configSerializer.Decode)
+
+    logger.LogInformation("Found config {config}", config)
+
+    let migrationsDir =
+      DirectoryInfo(Path.Combine(nonNull rootDir, config.migrations))
+
+    let! migrations =
+      migrationsDir.EnumerateFiles("*.sql", SearchOption.TopDirectoryOnly)
+      |> Seq.map(fun f -> asyncEx {
+        let! token = Async.CancellationToken
+        let! content = File.ReadAllTextAsync(f.FullName, token)
+        logger.LogDebug("Found migration {migrationPath}", f.FullName)
+        return migrationSerializer.DecodeText content
+      })
+      |> Async.Parallel
+
+    logger.LogInformation("Found migrations {migrations}", migrations.Length)
+
+    logger.LogInformation(
+      "Creating new virtual project {projectName}",
+      projectName
+    )
+
+    let! vProjectId =
+      vpr.InsertProject {
+        name = projectName
+        description = $"Imported from local ({projectName})"
+        connection = config.connection
+        tableName = config.tableName
+        driver = config.driver
+      }
+
+    let! migrations =
+      migrations
+      |> Array.map(fun migration -> asyncEx {
+        let! token = Async.CancellationToken
+
+        let vMigration = {
+          id = Guid.NewGuid()
+          name = migration.name
+          timestamp = migration.timestamp
+          upContent = migration.upContent
+          downContent = migration.downContent
+          manualTransaction = migration.manualTransaction
+          projectId = vProjectId
+        }
+
+        logger.LogDebug(
+          "Creating virtual migration {vMigration}",
+          vMigration.name
+        )
+
+        return! vpr.InsertMigration vMigration token
+
+      })
+      |> Async.Parallel
+
+    logger.LogInformation(
+      "Created virtual project {projectName} with {migrations} migrations",
+      projectName,
+      migrations.Length
+    )
+
+    return vProjectId
+  }
+
   { new MigrondiUIFs with
 
       member _.ListMigrations migrationsLocation = failwith "Not Implemented"
@@ -200,11 +287,105 @@ let getVirtualFs
           return! vpr.InsertMigration virtualMigration ct
         }
 
-      member _.ExportToLocal (project, path) = failwith "Not Implemented"
+      member _.ExportToLocal(project, path) = cancellableTask {
+        let! token = CancellableTask.getCancellationToken()
+
+        logger.LogInformation(
+          "Exporting project {project} to {path}",
+          project,
+          path
+        )
+
+        let! found = taskOption {
+          let! project = vpr.GetProjectById project token
+          let! migrations = vpr.GetMigrations project.id token
+          return project, migrations
+        }
+
+        match found with
+        | None ->
+          logger.LogWarning("Project with id {project} not found", project)
+          return failwith $"Project with id {project} not found"
+        | Some(project, migrations) ->
+          logger.LogInformation(
+            "Found project {project} with {migrations} migrations",
+            project,
+            migrations.Length
+          )
+
+          let config = {
+            project.ToMigrondiConfig() with
+                migrations = "./migrations"
+          }
+
+          let configSerializer, migrationSerializer =
+            let serializer = MigrondiSerializer()
+
+            serializer :> IMiConfigurationSerializer,
+            serializer :> IMiMigrationSerializer
+
+          let projectRoot = Directory.CreateDirectory(path)
+          let migrationsDir = projectRoot.CreateSubdirectory("migrations")
+          projectRoot.Create()
+          migrationsDir.Create()
+
+          logger.LogDebug(
+            "Created project root {projectRoot} and migrations dir {migrationsDir}",
+            projectRoot.FullName,
+            migrationsDir.FullName
+          )
+
+          let configPath = Path.Combine(projectRoot.FullName, "migrondi.json")
+          let configContent = configSerializer.Encode config
+
+          logger.LogInformation("Writing config to {configPath}", configPath)
+
+          do! File.WriteAllTextAsync(configPath, configContent, token)
+
+          logger.LogInformation(
+            "Writing migrations to {migrationsDir}",
+            migrationsDir.FullName
+          )
+
+          do!
+            migrations
+            |> List.map(fun vm -> async {
+              let! token = Async.CancellationToken
+
+              let migration = vm.ToMigration()
+              let content = migrationSerializer.EncodeText migration
+
+              let migrationPath =
+                Path.Combine(
+                  migrationsDir.FullName,
+                  $"{vm.timestamp}_{vm.name}.sql"
+                )
+
+              logger.LogDebug(
+                "Writing migration {migrationPath}",
+                migrationPath
+              )
+
+              do! File.WriteAllTextAsync(migrationPath, content, token)
+              return ()
+            })
+            |> Async.Parallel
+            |> Async.Ignore
+
+          logger.LogInformation(
+            "Project exported to {projectRoot}",
+            projectRoot.FullName
+          )
+
+          return projectRoot.FullName
+      }
 
       member _.ImportFromLocal(project: LocalProject) : CancellableTask<Guid> =
-        failwith "Not Implemented"
+        importProjectFromPath project.migrondiConfigPath project.name
 
-      member _.ImportFromLocal(projectPath: string) : CancellableTask<Guid> =
-        failwith "Not Implemented"
+      member _.ImportFromLocal(projectConfigPath: string) =
+
+        let projectName = Path.GetDirectoryName projectConfigPath |> nonNull
+
+        importProjectFromPath projectConfigPath projectName
   }
