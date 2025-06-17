2370df69c07577f322b6c0d00789684318bb7f9c
Angel D. Munoz <scythes.lol@gmail.com>
2025-06-17 14:44:44 -0600

    refactor(Fields): enhance styling for labeled fields
    - Added horizontal and vertical orientation styles for StackPanel.
    - Updated TextBlock styles to include bold font weight for labels.
    - Removed inline orientation methods in favor of dedicated styling functions.

--- src/MigrondiUI/Components/Fields.fs
+++ src/MigrondiUI/Components/Fields.fs
@@ -2,45 +2,82 @@
 
 open Avalonia.Controls
 open NXUI.Extensions
+open System
+
+[<AutoOpen>]
+module private Styles =
+  open Avalonia.Styling
+
+  type StackPanel with
+
+    member this.HorizontalFieldStyles() =
+      this.Styles.AddRange [
+        Style()
+          .Selector(fun s ->
+            s.OfType<StackPanel>().Class("LabeledField_Horizontal"))
+          .SetStackLayoutOrientation(Avalonia.Layout.Orientation.Horizontal)
+          .SetStackLayoutSpacing(4)
+        Style()
+          .Selector(fun s -> s.OfType<TextBlock>().Class("LabeledField_Label"))
+          .SetTextBlockFontWeight(Avalonia.Media.FontWeight.Bold)
+          .SetTextBlockFontSize(14)
+        Style()
+          .Selector(fun s -> s.OfType<TextBlock>().Class("LabeledField_Text"))
+          .SetTextBlockFontSize(12.)
+      ]
+
+      this
+
+    member this.VerticalFieldStyles() =
+      this.Styles.AddRange [
+        Style()
+          .Selector(fun s ->
+            s.OfType<StackPanel>().Class("LabeledField_Vertical"))
+          .SetStackLayoutOrientation(Avalonia.Layout.Orientation.Vertical)
+          .SetStackLayoutSpacing(4)
+        Style()
+          .Selector(fun s -> s.OfType<TextBlock>().Class("LabeledField_Label"))
+          .SetTextBlockFontWeight(Avalonia.Media.FontWeight.Bold)
+          .SetTextBlockFontSize(14)
+        Style()
+          .Selector(fun s -> s.OfType<TextBlock>().Class("LabeledField_Text"))
+          .SetTextBlockFontSize(12.)
+      ]
+
+      this
 
 type LabeledField =
 
-  static member inline Horizontal
-    (label: string, content: string, ?spacing: int)
-    =
+  static member Horizontal(label: string, content: string, ?spacing: int) =
     StackPanel()
       .Classes("LabeledField_Horizontal")
-      .OrientationHorizontal()
-      .Spacing(defaultArg spacing 4)
       .Children(
         TextBlock().Classes("LabeledField_Label").Text label,
         TextBlock().Classes("LabeledField_Text").Text content
       )
+      .HorizontalFieldStyles()
+      .Spacing(defaultArg spacing 4)
 
-  static member inline Horizontal
-    (label: string, content: Control, ?spacing: int)
-    =
+  static member Horizontal(label: string, content: Control, ?spacing: int) =
     StackPanel()
       .Classes("LabeledField_Horizontal")
-      .OrientationHorizontal()
-      .Spacing(defaultArg spacing 4)
       .Children(TextBlock().Classes("LabeledField_Label").Text label, content)
+      .HorizontalFieldStyles()
+      .Spacing(defaultArg spacing 4)
 
-  static member inline Vertical(label: string, content: string, ?spacing: int) =
+  static member Vertical(label: string, content: string, ?spacing: int) =
     StackPanel()
       .Classes("LabeledField_Vertical")
-      .OrientationVertical()
-      .Spacing(defaultArg spacing 4)
       .Children(
         TextBlock().Classes("LabeledField_Label").Text label,
         TextBlock().Classes("LabeledField_Text").Text content
       )
+      .VerticalFieldStyles()
+      .Spacing(defaultArg spacing 4)
 
-  static member inline Vertical
-    (label: string, content: Control, ?spacing: int)
-    =
+  static member Vertical(label: string, content: Control, ?spacing: int) =
     StackPanel()
       .Classes("LabeledField_Vertical")
-      .OrientationVertical()
-      .Spacing(defaultArg spacing 4)
       .Children(TextBlock().Classes("LabeledField_Label").Text label, content)
+      .VerticalFieldStyles()
+      .Spacing(defaultArg spacing 4)
